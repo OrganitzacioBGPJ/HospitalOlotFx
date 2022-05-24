5f6b15245b9dff2bb0faf17068896f282b5a09bc
Bernat23 <bpujolriu771@boscdelacoma.cat>
2022-05-24 14:45:09 +0200

    hospital mig

--- src/hospitalolot/model/persistence/dao/implementations/jdbc/utils/JDBCTipusGuardia.java
+++ src/hospitalolot/model/persistence/dao/implementations/jdbc/utils/JDBCTipusGuardia.java
@@ -4,8 +4,14 @@
  */
 package hospitalolot.model.persistence.dao.implementations.jdbc.utils;
 
-import hospitalolot.model.business.entities.Guardia;
+import hospitalolot.model.business.entities.Categoria;
+import hospitalolot.model.business.entities.Guardies;
+import hospitalolot.model.business.entities.Torn;
+import hospitalolot.model.business.entities.Unitat;
 import hospitalolot.model.persistence.dao.contract.Dao;
+import hospitalolot.model.persistence.dao.implementations.jdbc.JDBCCategoria;
+import hospitalolot.model.persistence.dao.implementations.jdbc.JDBCTorn;
+import hospitalolot.model.persistence.dao.implementations.jdbc.JDBCUnitat;
 import hospitalolot.model.persistence.dao.implementations.jdbc.mysql.MySQLConnection;
 import hospitalolot.model.persistence.exception.DAOException;
 import java.sql.ResultSet;
@@ -19,6 +25,10 @@ import java.util.List;
  * @author Bernat
  */
 public class JDBCTipusGuardia implements Dao{
+    
+    private static JDBCUnitat ju = new JDBCUnitat();
+    private static JDBCTorn jt = new JDBCTorn();
+    private static JDBCCategoria jc = new JDBCCategoria();
 
     @Override
     public Object get(long id) throws DAOException {
@@ -30,10 +40,12 @@ public class JDBCTipusGuardia implements Dao{
         try {
             Statement query = MySQLConnection.getInstance().getConnection().createStatement();
             ResultSet result = query.executeQuery("Select * from tipusguardia");
-            List<Guardia> llista = new ArrayList<>();
+            List<Guardies> llista = new ArrayList<>();
             while (result.next()) {
-                llista.add(new Guardia(result.getString("Horari"), result.getInt("idTorn")));
-               
+                Unitat u = ju.get(result.getInt("idUnitat"));
+                Torn t = jt.get(result.getInt("idTorn"));
+                Categoria c = jc.get(result.getInt("idCategoria"));
+                llista.add(new Guardies(u,t,c, result.getDate("dia").toLocalDate(), (byte)result.getInt("numTreballadors")));
             }
             return llista;
         } catch (SQLException ex) {
